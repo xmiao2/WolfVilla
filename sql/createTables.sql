3f3786c935da1cebd6b3799bb3b3bbeddd67dea0
Xiaohang Miao <xmiao2@ncsu.edu>
2016-10-03 02:52:33 -0400

    Changed manager_id to reference staff_id, changed manager and hotel to 1-to-1 relationship

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,3 +1,3 @@
-CREATE TABLE hotels (id int PRIMARY KEY, address varchar(50), name varchar(30), phone_number varchar(10), manager_id int NOT NULL FOREIGN KEY REFERENCES managers(id));
+CREATE TABLE hotels (id int PRIMARY KEY, address varchar(50), name varchar(30), phone_number varchar(10), manager_id int NOT NULL UNIQUE FOREIGN KEY REFERENCES managers(id));
 
-CREATE TABLE managers (id int PRIMARY KEY);
+CREATE TABLE managers (id int PRIMARY KEY REFERENCES staff(id));
