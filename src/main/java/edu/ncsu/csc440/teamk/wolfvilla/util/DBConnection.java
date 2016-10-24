8e1cebaaa6212542da405e0bc50cda1be277e1ee
xmiao2 <xmiao2@ncsu.edu>
2016-10-24 19:59:26 -0400

    Reworked DBConnection

--- src/main/java/edu/ncsu/csc440/teamk/wolfvilla/util/DBConnection.java
+++ src/main/java/edu/ncsu/csc440/teamk/wolfvilla/util/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private DBConnection dbConnection;
-    private Connection connection;
+    private static DBConnection dbConnection;
+    private static Connection connection;
 
     private static final String JDBC_URL = "jdbc:oracle:thin:@//orca.csc.ncsu.edu:1521/orcl.csc.ncsu.edu";
     private static final String USERNAME = "xmiao2";
@@ -18,14 +18,10 @@ public class DBConnection {
         connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
 
-    public synchronized DBConnection getInstance() throws SQLException, ClassNotFoundException {
+    public synchronized static Connection getConnection() throws SQLException, ClassNotFoundException {
         if (dbConnection == null) {
             dbConnection = new DBConnection();
         }
-        return dbConnection;
-    }
-
-    public Connection getConnection() {
         return connection;
     }
 }
