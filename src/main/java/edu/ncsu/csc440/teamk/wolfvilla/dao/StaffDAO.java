547694983c84036ad02fb914f91dc57509f026a7
Joshua Cook <jacook7@ncsu.edu>
2016-10-25 17:02:20 -0400

    I added functions for updating, deleting, and querying staff.

--- src/main/java/edu/ncsu/csc440/teamk/wolfvilla/dao/StaffDAO.java
+++ src/main/java/edu/ncsu/csc440/teamk/wolfvilla/dao/StaffDAO.java
@@ -4,6 +4,8 @@ import edu.ncsu.csc440.teamk.wolfvilla.model.Staff;
 import edu.ncsu.csc440.teamk.wolfvilla.util.DBConnection;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xm on 10/24/16.
@@ -20,6 +22,26 @@ public class StaffDAO {
             return null;
         }
         return c.toString();
+    }
+
+    public static void updateStaff(Staff staff) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getConnection();
+
+
+        PreparedStatement stmt = connection.prepareStatement(
+                "UPDATE staff SET name=?, job_title=?, ssn=?, age=?, gender=?, address=?, phone_number=?, hotel_id=? " +
+                        "WHERE id = ?");
+        stmt.setString(1,staff.getName());
+        stmt.setString(2,staff.getTitle());
+        stmt.setString(3,staff.getSsn());
+        stmt.setInt(4,staff.getAge());
+        stmt.setString(5, charToString(staff.getGender()));
+        stmt.setString(6, staff.getAddress());
+        stmt.setString(7, staff.getPhoneNumber());
+        stmt.setLong(8, staff.getHotelId());
+        stmt.setLong(9, staff.getId());
+
+        stmt.executeUpdate();
     }
 
     public static long addStaff(Staff staff) throws SQLException, ClassNotFoundException {
@@ -53,6 +75,40 @@ public class StaffDAO {
         ResultSet rs = stmt.executeQuery();
         rs.next();
         return convertToStaff(rs);
+    }
+
+    public static void deleteStaff(long staffID) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getConnection();
+
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM staff WHERE id = ?");
+        stmt.setLong(1,staffID);
+        stmt.executeUpdate();
+    }
+
+    public static List<Staff> retrieveStaffByHotel(long hotelID) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM staff WHERE hotel_id = ?");
+        stmt.setLong(1,hotelID);
+        ResultSet rs = stmt.executeQuery();
+        return convertStaffList(rs);
+    }
+
+
+    public static List<Staff> retrieveStaffByTitle(long hotelID, long title) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM staff WHERE hotel_id = ? AND title = ?");
+        stmt.setLong(1,hotelID);
+        stmt.setLong(2,title);
+        ResultSet rs = stmt.executeQuery();
+        return convertStaffList(rs);
+    }
+
+    private static List<Staff> convertStaffList(ResultSet rs) throws SQLException {
+        ArrayList<Staff> toReturn = new ArrayList<Staff>();
+        while(rs.next()) {
+            toReturn.add(convertToStaff(rs));
+        }
+        return toReturn;
     }
 
     private static Staff convertToStaff(ResultSet rs) throws SQLException {
